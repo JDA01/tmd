59b983e1191f1aba6805437ef70b3dd4b9b56c50
juan <01juandalzate01@gmail.com>
2021-03-25 03:04:22 -0400

    add responsive view

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,33 +1,49 @@
 const toggler = document.querySelector(".toggler");
 const burger = document.querySelector(".burger");
-// const navigation = document.querySelector(".navigation");
-const mobileNav = document.querySelector("#mobile-nav");
+const mobileMenu = document.querySelector("#mobile-menu");
+const nav = document.querySelector("nav");
 
 
-toggler.addEventListener("click", function(){
-    burger.classList.toggle("open");
-    // navigation.classList.toggle("active");
-    mobileNav.classList.toggle("width");
+toggler.addEventListener("click", function () {
+  burger.classList.toggle("open");
+  mobileMenu.classList.toggle("width");
 });
 
+// HIDE NAVBAR ON SCROLL DOWN
+let prevScrollpos = window.pageYOffset;
 
+window.onscroll = function () {
+  
+
+  let currentScrollPos = window.pageYOffset;
+  
+
+  if (prevScrollpos > currentScrollPos) {
+    nav.style.top = "0";
+  } else {
+    nav.style.top = "-1000px";
+  }
+  prevScrollpos = currentScrollPos;
+
+  
+}
 
 
 // Initialize Swiper 
 
-  var swiper = new Swiper('.swiper-container', {
-    spaceBetween: 30,
-    effect: 'fade',
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+var swiper = new Swiper('.swiper-container', {
+  spaceBetween: 30,
+  effect: 'fade',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
